5e024b9bbeac7d39668f76964597b033064229a5
Anamaleit <anamaleit@gmail.com>
2022-12-10 10:34:33 +0700

    user controller + authentication backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AnnouncementsContextProvider } from './context/AnnouncementContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AnnouncementsContextProvider>
-        <App/>
-    </AnnouncementsContextProvider>
+    <AuthContextProvider>
+      <AnnouncementsContextProvider>
+          <App/>
+      </AnnouncementsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
