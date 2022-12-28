03b1684b9ba1ca7ced7c711356b961db0ffa38fd
sfz <>
2022-12-27 19:16:20 -0800

    navbar admin support

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,7 +15,7 @@ const loginUser = async (req, res) => {
     // create a token
     const token = createToken(user._id)
 
-    res.status(200).json({email, token})
+    res.status(200).json({email, token, isTeacher: user.isTeacher, isAdmin: user.isAdmin})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
@@ -31,7 +31,7 @@ const signupUser = async (req, res) => {
     // create a token
     const token = createToken(user._id)
 
-    res.status(200).json({email, token})
+    res.status(200).json({email, token, isTeacher: user.isTeacher, isAdmin: user.isAdmin})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -10,33 +10,73 @@ const Navbar = () => {
         logout()
     }
 
-    return (
-        <header>
-            {user && (
-            <div className="nav-left">
-                <ul>
-                    <li><Link to = "/">Dashboard</Link></li>
-                    <li><Link to = "/Raw">Raw</Link></li>
-                    <li><Link to = "/Student">Student</Link></li>
-                    <li><Link to ="/Nilai">Nilai</Link></li>
-                    <li><Link to="/Absensi">Absensi</Link></li>
-                    <li><Link to="/StatusPembayaran">Status Pembayaran</Link></li>
-                    <li><Link to="/Keterangan">Keterangan</Link></li>
-                    <li style={{marginLeft: "600px"}}>{user.email}</li>
-                    <button onClick={handleClick} style={{marginLeft: "10px"}}>Log out</button>
-                </ul>
-            </div>
-            )}
-            {!user && (
-            <div className="nav-right">
-                <ul>
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/signup">Signup</Link></li>
-                </ul>
-            </div>
-            )}
-        </header>
-    )
+    // non-user
+    if (!user){
+        return (
+            <header>
+                <div className="nav-right">
+                    <ul>
+                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/signup">Signup</Link></li>
+                    </ul>
+                </div>
+            </header>
+        );
+    }
+    // admin
+    else if (user && user.isAdmin){
+        return (
+            <header>
+                <div className="nav-left">
+                    <ul>
+                        <li><Link to="/">Dashboard</Link></li>
+                        <li><Link to="/Raw">Raw</Link></li>
+                        <li><Link to="/Student">Student</Link></li>
+                        <li><Link to="/Nilai">Nilai</Link></li>
+                        <li><Link to="/Absensi">Absensi</Link></li>
+                        <li><Link to="/StatusPembayaran">Status Pembayaran</Link></li>
+                        <li><Link to="/Keterangan">Keterangan</Link></li>
+                        <li style={{marginLeft: "600px"}}>{user.email}</li>
+                        <button onClick={handleClick} style={{marginLeft: "10px"}}>Log out</button>
+                    </ul>
+                </div>
+            </header>
+        );
+    }
+    // teacher
+    else if (user && user.isTeacher){
+        return (
+            <header>
+                <div className="nav-left">
+                    <ul>
+                        <li><Link to="/">Dashboard</Link></li>
+                        <li><Link to="/Student">Student</Link></li>
+                        <li><Link to="/Nilai">Nilai</Link></li>
+                        <li><Link to="/Absensi">Absensi</Link></li>
+                        <li><Link to="/StatusPembayaran">Status Pembayaran</Link></li>
+                        <li><Link to="/Keterangan">Keterangan</Link></li>
+                        <li style={{marginLeft: "600px"}}>{user.email}</li>
+                        <button onClick={handleClick} style={{marginLeft: "10px"}}>Log out</button>
+                    </ul>
+                </div>
+            </header>
+        );
+    }
+    // unprivileged user
+    else{
+        return (
+            <header>
+                <div className="nav-left">
+                    <ul>
+                        <li><Link to="/">Dashboard</Link></li>
+                        <li><Link to="/Student">Student</Link></li>
+                        <li style={{marginLeft: "600px"}}>{user.email}</li>
+                        <button onClick={handleClick} style={{marginLeft: "10px"}}>Log out</button>
+                    </ul>
+                </div>
+            </header>
+        );
+    }
 }
 
 export default Navbar
